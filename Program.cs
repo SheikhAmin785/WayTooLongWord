4154ff0a1bccbdb42e8e2f5d1698d485b81ceb79
SheikhAmin785 <shakil785ks@gmail.com>
2023-10-18 21:33:53 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WayTooLongWord
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+           for(int i = 0; i < n; i++)
+            {
+                string x= Console.ReadLine();
+                int length = x.Length;
+                if (length <= 10)
+                {
+                    Console.WriteLine(x);                }
+                else
+                {
+                    Console.WriteLine($"{x[0]}{length - 2}{x[length-1]}");
+
+                }
+
+            }
+            
+        }
+    }
+}
